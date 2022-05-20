61191b6505cce8905d19d0f14b9a086d57f6f563
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2022-05-21 00:29:25 +0200

    fix: tiptap

--- src/lib/stores/localStorage.ts
+++ src/lib/stores/localStorage.ts
@@ -1,8 +1,7 @@
-import type { JSONContent } from '@tiptap/core';
 import { writable } from 'svelte-local-storage-store';
 
 type Notepad = {
 	name: string;
-	body: JSONContent;
+	body: any;
 };
 export const notepad = writable<Notepad[]>('notepad', [{ name: 'File 1', body: {} }]);
