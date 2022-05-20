6f33cf284a71d717d8efff36be93ec2c3a294583
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2022-05-21 01:14:35 +0200

    Update playlist

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,10 +1,14 @@
 const chopinPlaylist = {
 	name: 'Chopin',
 	tracks: [
-		{ id: 'Jn09UdSb3aA', title: 'Chopin FM' },
-		{ id: 'tGA1v6gZj1s', title: 'Radio Bach' },
-		{ id: 'Rb0UmrCXxVA', title: '101 Mozart' },
-		{ id: 'W-fFHeTX70Q', title: 'Beethoven City' }
+		{ id: 'Jn09UdSb3aA', title: 'The Best of Chopin' },
+		{ id: 'tGA1v6gZj1s', title: '50 Best of Bach' },
+		{ id: 'Rb0UmrCXxVA', title: 'The Best of Mozart' },
+		{ id: 'W-fFHeTX70Q', title: 'The Best of Beethoven' },
+		{ id: 'Y56ODjb8ZMo', title: "You're a hopeless romantic but in the 19th century" },
+		{ id: 'JCbaIOz9NOA', title: 'Uplifting, Inspiring & Motivational Classical Music' },
+		{ id: 'aMJyQK4xBUQ', title: 'Oldies playing in another room' },
+		{ id: '6bmZgzBu9pY', title: "Tom & Jerry's best hits" }
 	]
 };
 export const playlists = {
